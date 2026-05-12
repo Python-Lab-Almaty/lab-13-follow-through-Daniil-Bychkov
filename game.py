b132241fa37ba8551855a8368c86300920a86d95
Daniil-Bychkov <daniil.bychkov@std.agakaz.kz>
2026-05-12 13:58:39 +0500

    Task 6: Change Bgcolor to lightblue [PASS]

--- game.py
+++ game.py
@@ -133,7 +133,7 @@ start, goal, impassable_obstacles = load_or_create_positions(student_name)
 screen = turtle.Screen()
 screen.setup(WIDTH, HEIGHT)
 screen.title(f"Red Riding Hood Mission - {student_name}")
-screen.bgcolor("white")
+screen.bgcolor("lightblue")
 screen.tracer(0)
 
 # ----------------------------
